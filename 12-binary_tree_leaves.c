60b70f9801408c3e830b5c94436787372a43bd11
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-01-03 10:40:31 +0200

    fix

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -15,8 +15,12 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	leaves = 0;
 	if (!tree)
 		return (0);
-	leaves += binary_tree_is_leaf(tree);
-	leaves += binary_tree_leaves(tree->left);
-	leaves += binary_tree_leaves(tree->right);
+	if (!(tree->left && tree->right))
+		leaves++;
+	else
+	{
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
+	}
 	return (leaves);
 }
