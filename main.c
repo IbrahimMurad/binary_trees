d135dd9a3f6b640aa8f329a3096302a1ce71071a
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-01-03 14:43:21 +0200

    is_perfect

--- main.c
+++ main.c
@@ -13,14 +13,33 @@ int main(void)
     int perfect;
 
     root = binary_tree_node(NULL, 98);
+    binary_tree_print(root);
     perfect = binary_tree_is_perfect(root);
     printf("Perfect: %d\n", perfect);
     root->left = binary_tree_node(root, 12);
+    binary_tree_print(root);
+    perfect = binary_tree_is_perfect(root);
+    printf("Perfect: %d\n", perfect);
     root->right = binary_tree_node(root, 402);
+    binary_tree_print(root);
+    perfect = binary_tree_is_perfect(root);
+    printf("Perfect: %d\n", perfect);
     binary_tree_insert_right(root->left, 54);
+    binary_tree_print(root);
+    perfect = binary_tree_is_perfect(root);
+    printf("Perfect: %d\n", perfect);
     binary_tree_insert_right(root, 128);
+    binary_tree_print(root);
+    perfect = binary_tree_is_perfect(root);
+    printf("Perfect: %d\n", perfect);
     root->left->left = binary_tree_node(root->left, 10);
+    binary_tree_print(root);
+    perfect = binary_tree_is_perfect(root);
+    printf("Perfect: %d\n", perfect);
     root->right->left = binary_tree_node(root->right, 10);
+    binary_tree_print(root);
+    perfect = binary_tree_is_perfect(root);
+    printf("Perfect: %d\n", perfect);
 
     binary_tree_print(root);
     perfect = binary_tree_is_perfect(root);
